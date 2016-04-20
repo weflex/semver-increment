05049a970fbb60f94b522114b4f5debe33bfee2d
jkvim <jkvim@outlook.com>
2016-04-20 18:12:11 +0800

    first patch

--- index.js
+++ index.js
@@ -17,6 +17,35 @@ var fs     = require('fs');
   */ 
 
 module.exports = function (masks, version) {
+  "use strict";
 
-  return '0.0.0';
+  if (typeof masks === 'string') {
+    version = masks;
+    masks = [0, 0, 0];
+  }
+
+  let bitMap = ['major', 'minor', 'patch'];
+  let bumpAt = 'patch';
+  let oldVer = version.match(/\d+/g);
+
+  for (let i = 0; i < masks.length; ++i) {
+    if (masks[i] === 1) {
+      bumpAt = bitMap[i];
+      break;
+    }
+  }
+
+  let bumpIdx = bitMap.indexOf(bumpAt);
+  let newVersion = []
+  for (let i = 0; i < oldVer.length; ++i) {
+    if (i < bumpIdx) {
+      newVersion[i] = +oldVer[i];
+    } else if (i === bumpIdx) {
+      newVersion[i] = +oldVer[i] + 1;
+    } else {
+      newVersion[i] = 0;
+    }
+  }
+
+  return newVersion.join('.');
 }
