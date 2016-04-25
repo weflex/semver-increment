4e618fde8eeb03d35832e9be3333332b03955e9d
ZeHang <jkvim@outlook.com>
2016-04-25 16:56:13 +0800

    add coverage report (#4)

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const gulp = require('gulp');
+const jasmine = require('gulp-jasmine');
+const istanbul = require('gulp-istanbul');
+const SpecReporter = require('jasmine-spec-reporter');
+
+let srcs = gulp.src('./index.js');
+let all = gulp.src('./spec/*.js');
+
+function buildCoverage(argument) {
+  return srcs
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire());
+}
+
+function runSpecs(argument) {
+  let spec;
+  let toJasmine = jasmine({
+    reporter: new SpecReporter({
+      displayStackTrace: 'summary'
+    })
+  }); 
+
+  spec = all.pipe(toJasmine)
+    .pipe(istanbul.writeReports())
+    .pipe(istanbul.enforceThresholds({
+      thresholds: {global: 50}
+    }));
+
+  return spec.on('end', function () {
+    process.exit(0);
+  }).on('error', function () {
+    process.exit(1);
+  });
+}
+
+gulp.task('buildCoverage', buildCoverage);
+gulp.task('specs', ['buildCoverage'], runSpecs);
