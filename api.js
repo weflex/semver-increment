cc851f3ca2865a9404e664c3c6c0d79656bb20b1
jkvim <jkvim@outlook.com>
2016-04-26 21:47:00 +0800

    docs

---
+++ api.js
@@ -0,0 +1,11 @@
+window.apimeta = {
+  "enums": [],
+  "classes": [
+    {
+      "name": "SemverIncrement",
+      "namespace": "SemverIncrement",
+      "description": "<p>Bump semantic version (SemVer) number in VERSION according to MASKS. MASKS\nis an array composed of three digits, e.g. [0, 1, 0]. The topmost\n(left-most) nonzero bit of SemVer number is bumped (incremented) and the\nlower bits are set to zeros.</p>\n"
+    }
+  ],
+  "modules": []
+}
